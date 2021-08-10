ac4f7241694bff16533ca54856cac7129711569f
Pablo de la Peña <p@shedcollective.org>
2021-08-10 17:05:27 +0100

    Initial commit of stubs

---
+++ src/Constants.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nails\ReleaseNotes;
+
+/**
+ * Class Constants
+ *
+ * @package Nails\ReleaseNotes
+ */
+class Constants
+{
+    /**
+     * The slug for this module
+     *
+     * @var string
+     */
+    const MODULE_SLUG = 'nails/module-release-notes';
+}
