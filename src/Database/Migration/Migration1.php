ba46f6fb8d4753986b8573a8963328de83937ec9
Pablo de la Peña <p@shedcollective.org>
2024-07-05 13:24:13 +0100

    chore: Tolerate null value

--- src/Database/Migration/Migration1.php
+++ src/Database/Migration/Migration1.php
@@ -51,7 +51,7 @@ class Migration1 implements Interfaces\Database\Migration
         $oResult = $this->query('SELECT id, acl FROM `{{NAILS_DB_PREFIX}}user_group`');
         while ($row = $oResult->fetchObject()) {
 
-            $acl = json_decode($row->acl) ?? [];
+            $acl = json_decode($row->acl ?? 'null') ?? [];
 
             foreach ($acl as &$old) {
                 $old = self::MAP[$old] ?? $old;
