de84f9ca511b2c341d375c341645466b26b0b7e1
Pablo de la Peña <p@shedcollective.org>
2025-05-20 11:46:54 +0100

    chore: Ensure button only renders for those with permission

--- src/Admin/Dashboard/Widget/ReleaseNotes.php
+++ src/Admin/Dashboard/Widget/ReleaseNotes.php
@@ -132,11 +132,11 @@ class ReleaseNotes implements Interfaces\Dashboard\Widget
         return implode(PHP_EOL, [
             $sStyles,
             implode(PHP_EOL, $aTags),
-            sprintf(
+            userHasPermission('admin:releasenotes:browse') ? sprintf(
                 '<p class="%s"><a href="%s" class="btn btn-primary btn-block">View All</a></a></p>',
                 'release-notes-' . $sGuid . '--cta',
                 siteUrl('admin/releaseNotes/releaseNotes/index')
-            ),
+            ) : '',
         ]);
     }
 
